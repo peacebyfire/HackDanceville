e8165afb5d32fbbf07a1b82f0a0aaa2f4f402ecc
Will Gaggioli <wgaggioli@gmail.com>
2013-10-18 22:38:10 -0500

    add optional size parameter

--- hackdanceville/api.py
+++ hackdanceville/api.py
@@ -9,12 +9,13 @@ class InvalidData(Exception):
 
 class DancefloorAPI(object):
 
-    def __init__(self, ip_address, port, validate=False):
+    def __init__(self, ip_address, port, size=64, validate=False):
         self.ip_address = ip_address
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.connected = False
         self.ENDDATA = bytearray(list('DANCEFLOOR') + [2])
+        self.size = size
         self.validate = validate
 
     def connect(self):
@@ -22,9 +23,9 @@ class DancefloorAPI(object):
         self.connected = True
 
     def validate_array(self, array):
-        if not len(array) in (64, 192):
+        if not len(array) in (self.size, self.size * 3):
             raise InvalidData('array must be length 64 or 192')
-        if len(array) == 64:
+        if len(array) == self.size:
             for el in array:
                 if len(el) != 3:
                     raise InvalidData('Element %s not length 3' % el)
