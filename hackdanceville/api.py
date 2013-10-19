69c6365da681b03a7c4da76ae33a5b86353ea657
Will Gaggioli <wgaggioli@gmail.com>
2013-10-18 22:16:05 -0500

    move api to [(r0, g0, b0), ...]

--- hackdanceville/api.py
+++ hackdanceville/api.py
@@ -1,15 +1,17 @@
 import time
 import socket
+from itertools import chain
 
 
 class DancefloorAPI(object):
 
-    def __init__(self, ip_address, port):
+    def __init__(self, ip_address, port, validate=False):
         self.ip_address = ip_address
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.connected = False
         self.ENDDATA = bytearray(list('DANCEFLOOR') + [2])
+        self.validate = validate
 
     def connect(self):
         self.sock.connect((self.ip_address, self.port))
@@ -19,15 +21,15 @@ class DancefloorAPI(object):
         pass
 
     def convert_array(self, array):
-        data = ['D', 'A', 'N', 'C', 'E', 'F', 'L', 'O', 'O', 'R', 1] + array
-        raw = bytearray(data)
+        raw = bytearray(chain('DANCEFLOOR', [1], array))
         return raw
 
     def _send_raw(self, bytes):
         return self.sock.sendto(bytes, (self.ip_address, self.port))
 
     def send(self, array):
-        self.validate_array(array)
+        if self.validate:
+            self.validate_array(array)
         raw = self.convert_array(array)
         self._send_raw(raw)
         self._send_raw(self.ENDDATA)
