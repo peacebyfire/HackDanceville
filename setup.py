7e9743910c6ff67f0f9b0a1a21c7c41f7a749110
Will Gaggioli <wgaggioli@gmail.com>
2013-10-18 21:00:23 -0500

    setup.py and initdir

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name='HackDanceville',
+    version='0.1',
+    description='Hack til you drop',
+    author_email='',
+    url='',
+    install_requires=[
+
+    ],
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+)
