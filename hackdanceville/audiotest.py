ea2559510b0c04874d348c69cde8a0202d67847f
clint <clinth505@yahoo.com>
2013-10-19 00:49:13 -0400

    audiotest.py

---
+++ hackdanceville/audiotest.py
@@ -0,0 +1,97 @@
+import pyaudio
+import wave
+import numpy
+import sys
+import time
+from hackdanceville.api import dancefloor
+
+# Should add some junk from here: https://github.com/rezaali/ofxBeatDetector/tree/master/src
+
+class SimpleBeatDetection:
+    """
+    Simple beat detection algorithm from
+    http://archive.gamedev.net/archive/reference/programming/features/beatdetection/index.html
+    """
+    def __init__(self, history = 43):
+        self.local_energy = numpy.zeros(history) # a simple ring buffer
+        self.local_energy_index = 0 # the index of the oldest element
+
+    def detect_beat(self, signal):
+
+        samples = signal.astype(numpy.int) # make room for squares
+        # optimized sum of squares, i.e faster version of (samples**2).sum()
+        instant_energy = numpy.dot(samples, samples) / float(0xffffffff) # normalize
+
+        local_energy_average = self.local_energy.mean()
+        local_energy_variance = self.local_energy.var()
+
+        beat_sensibility = (-0.0025714 * local_energy_variance) + 1.15142857
+        beat = instant_energy > beat_sensibility * local_energy_average
+
+        self.local_energy[self.local_energy_index] = instant_energy
+        self.local_energy_index -= 1
+        if self.local_energy_index < 0:
+            self.local_energy_index = len(self.local_energy) - 1
+
+        return beat
+
+
+###################################################################
+
+beat_detector = SimpleBeatDetection()
+#dancefloor = DancefloorAPI('192.168.1.2', 21337)
+
+#https://github.com/bastibe/PyAudio/blob/master/test/play_wave_callback.py
+
+"""PyAudio Example: Play a wave file (callback version)."""
+
+if len(sys.argv) < 2:
+    print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
+    sys.exit(-1)
+
+wf = wave.open(sys.argv[1], 'rb')
+
+# instantiate PyAudio (1)
+p = pyaudio.PyAudio()
+
+# define callback (2)
+def callback(in_data, frame_count, time_info, status):
+    data = wf.readframes(frame_count)
+    ### GOOD STUFF HERE!!!
+    signal = numpy.frombuffer(data, numpy.int16)
+    beat = beat_detector.detect_beat(signal)
+    floor_data = []
+    if beat:
+        floor_data = [[0, 0, 0] for i in xrange(64)]
+    else:
+        floor_data = [[255, 255, 255] for i in xrange(64)]
+    dancefloor.send(floor_data)
+    print beat
+    return (data, pyaudio.paContinue)
+
+# open stream using callback (3)
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True,
+                stream_callback=callback)
+
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True)
+
+# start the stream (4)
+stream.start_stream()
+
+# wait for stream to finish (5)
+while stream.is_active():
+    time.sleep(0.1)
+
+# stop stream (6)
+stream.stop_stream()
+stream.close()
+wf.close()
+
+# close PyAudio (7)
+p.terminate()
