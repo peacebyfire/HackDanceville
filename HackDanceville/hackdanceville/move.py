97883561606a14b9a45fae3f50f1cccaefe55b6e
Jacques Woodcock <jacques@kitewebconsulting.com>
2013-10-19 19:41:24 -0500

    Started process of adding 2 players and a separate bomb class.

--- HackDanceville/hackdanceville/move.py
+++ HackDanceville/hackdanceville/move.py
@@ -2,12 +2,12 @@ import curses
 
 from hackdanceville.queue import DancefloorLoop
 
-
 def movement_wrapper(func):
     def wrapper(inst, *args, **kwargs):
         inst.data[inst.y * 8 + inst.x][inst.color_i] = 0
         func(inst, *args, **kwargs)
         inst.data[inst.y * 8 + inst.x][inst.color_i] = 255
+        # check to see if player is on top of bomb
     return wrapper
 
 
@@ -15,11 +15,12 @@ class Move(object):
 
     def __init__(self):
         self.init_data()
+        self.init_bomb()
         self.keymap = {
-            curses.KEY_UP: self.move_up,
-            curses.KEY_DOWN: self.move_down,
-            curses.KEY_LEFT: self.move_left,
-            curses.KEY_RIGHT: self.move_right,
+            curses.KEY_UP: self.player1_move_up,
+            curses.KEY_DOWN: self.player1_move_down,
+            curses.KEY_LEFT: self.player1_move_left,
+            curses.KEY_RIGHT: self.player1_move_right,
             99: self.change_color,
             113: self.quit
         }
@@ -28,27 +29,35 @@ class Move(object):
         self.go = False
 
     def init_data(self):
-        self.data = [[0, 255, 0]]
+        self.player1Data = [[0, 255, 0]]
         for i in xrange(63):
-            self.data.append([0, 0, 0])
-        self.x = 0
-        self.y = 0
+            self.player1Data.append([0, 0, 0])
+        self.player2Data = []
+        for i in xrange(63):
+            self.player2Data.append([0, 0, 0])
+        self.player2Data.append([0, 255, 0])
+        self.player1X = 0
+        self.player1Y 0
+        self.player2X = 0
+        self.player2Y 0
+        self.player1Bomb = Bomb();
+        self.player2Bomb = Bomb();
 
     @movement_wrapper
-    def move_left(self):
-        self.x = (self.x - 1) % 8
+    def player1_move_left(self):
+        self.player1X = (self.player1X - 1) % 8
 
     @movement_wrapper
-    def move_right(self):
-        self.x = (self.x + 1) % 8
+    def player1_move_right(self):
+        self.player1X = (self.player1X + 1) % 8
 
     @movement_wrapper
-    def move_up(self):
-        self.y = (self.y - 1) % 8
+    def player1_move_up(self):
+        self.player1Y = (self.player1Y - 1) % 8
 
     @movement_wrapper
-    def move_down(self):
-        self.y = (self.y + 1) % 8
+    def player1_move_down(self):
+        self.player1Y = (self.player1Y + 1) % 8
 
     @movement_wrapper
     def change_color(self):
@@ -56,12 +65,13 @@ class Move(object):
 
     def initialize_loop(self):
         if not self.loop or not self.loop.is_alive():
-            self.loop = DancefloorLoop(data=self.data)
+            self.loop = DancefloorLoop(data=self.player1Data)
             self.loop.start()
             self.go = True
 
     def quit(self):
-        self.data = None
+        self.player1Data = None
+        self.player2Data = None
         self.go = False
 
     def put(self, key):
@@ -70,7 +80,7 @@ class Move(object):
             func = self.keymap[key]
             func()
             print 'ran ' + str(func)
-        self.loop.queue.put(self.data)
+        self.loop.queue.put(self.player1Data)
 
     def curses_loop(self):
         stdscr = curses.initscr()
@@ -89,6 +99,40 @@ class Move(object):
             self.put(key)
 
         curses.endwin()
+
+class Bomb(object):
+
+    self.x = 0
+    self.y = 0
+    self.exploded = False
+
+    def __init__(self):
+        self.init_bomb()
+
+    def init_bomb(self):
+        self.bombData = []
+        for i in xrange(64):
+            self.bombData.append([0, 0, 0])
+        self.blasted = []
+
+    def collision(self, playerPosX, playerPosY):
+        if self.explosded == True AND self.x == playerPosX AND self.y == playerPosY:
+            return True;
+        else:
+            return False;
+
+    def setBomb(self, playerPosX, playerPosY):
+        self.x = playerPosX
+        self.y = playerPosY
+        # set to data and render
+        # start loop
+
+    def bombTimer(self):
+        # start the timer
+        # blink
+
+    def explodeBomb(self):
+        # 
 
 
 if __name__ == "__main__":
