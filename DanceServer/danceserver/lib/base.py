1bb420a5b73c0c9b992d44c9d2618072b61a5dfc
Will Gaggioli <wgaggioli@gmail.com>
2013-10-19 19:48:22 -0500

    lib module

---
+++ DanceServer/danceserver/lib/base.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""The base Controller API."""
+
+from tg import TGController, tmpl_context
+from tg.render import render
+from tg.i18n import ugettext as _, ungettext
+
+__all__ = ['BaseController']
+
+
+class BaseController(TGController):
+    """
+    Base class for the controllers in the application.
+
+    Your web application should have one of these. The root of
+    your application is used to compute URLs used by your app.
+
+    """
+
+    def __call__(self, environ, context):
+        """Invoke the Controller"""
+        # TGController.__call__ dispatches to the Controller method
+        # the request is routed to.
+
+        return TGController.__call__(self, environ, context)
+
+
+class BasePluginController(BaseController):
+    def __init__(self, api):
+        self.api = api
